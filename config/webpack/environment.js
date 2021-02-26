9c9d2b31d78e35e18e279192bd240a4d278999c3
Evgeny <evgeny.agafonov@dualbootpartners.com>
2021-02-26 18:28:27 +0400

    Add bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// Adds `var jQuery = require('jquery') to legacy jQuery plugins
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  }),
+)
 
 module.exports = environment
